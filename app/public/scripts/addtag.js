aece899018017dac2467db1d6e1a212e135b5260
Rebecca Weinberger <rew@mit.edu>
2017-01-20 21:28:54 -0500

    functional tags

--- app/public/scripts/addtag.js
+++ app/public/scripts/addtag.js
@@ -1,19 +1,38 @@
+var tags = [];
+
 $("#addtag").on("click", function() {
   var text = $("#inserttag").val();
   var id = Math.floor((Math.random() * 100000) + 1);
+  tags.push(text);
   $("#tags").append(
-    "<div class='tag' id='tag" + id+"'>"
+    "<div class='tag' id='tag"+id+"'><div class='tagtext' id='tagtext"+id+"'>"
     +text+
-    "&#32;<button id='closetag"
+    "</div>&#32;<button id='closetag"
     +id+
     "' class='closetag' type='button'>&#10005;</button></div>");
+  console.log("tags: "+tags);
 });
 
 $(document).on('click', ".closetag", function() {
-  console.log('hi');
   var id = $(this).attr('id');
-  var idnum = id.substring(8)
-  console.log('closing id #tag'+id);
+  var idnum = id.substring(8);
+  var tagtext = $("#tagtext"+idnum).text();
+  var index = tags.indexOf(tagtext);
+  tags.splice(index,1);
+  console.log('closing tag '+idnum);
   $("#tag"+idnum).remove();
-  console.log("did it work");
+  console.log("tag removed: "+tagtext);
+  console.log("new tag array: "+ tags);
 });
+
+$('#submititem').on('click', function(){
+  console.log(tags);
+  $.ajax({url:'/uploaditem', type: 'POST', data: {
+    itemname:$('#itemname').val(),
+    price:$('#price').val(),
+    description:$('#description').val(),
+    tags:tags,}, success: function(data) {
+      window.location.href = data
+    }
+  });
+})
