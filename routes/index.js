dad16d9940f8cbc2d641508f1d6d399d7f270aaa
Reva Ranka <revaranka@gmail.com>
2017-01-29 18:40:54 -0500

    home changes

--- routes/index.js
+++ routes/index.js
@@ -512,7 +512,7 @@ router.post('/forgot', function(req, res, next) {
 router.get('/reset/:token', function(req, res) {
   User.findOne({ resetPasswordToken: req.params.token, resetPasswordExpires: { $gt: Date.now() } }, function(err, user) {
     if (!user) {
-      req.flash('error', 'Password reset token is invalid or has expired.');
+      // req.flash('error', 'Password reset token is invalid or has expired.');
       return res.redirect('/forgot');
     }
     res.render('reset', {
