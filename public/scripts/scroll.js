dc87e0dcff1fcbd6ef0021015e00ed52369808eb
Rebecca Weinberger <rew@mit.edu>
2017-01-30 18:26:59 -0500

    login forgot pw scroll

--- public/scripts/scroll.js
+++ public/scripts/scroll.js
@@ -3,3 +3,10 @@ $("#scrolltoabout").click(function() {
       scrollTop: $("#about").offset().top
   }, 1500);
 });
+
+$(".twobuttonsfog").click(function() {
+  console.log('hi');
+  $('.modal-content').animate({
+      scrollTop: $(".twobuttonsfog").offset().top
+  }, 800);
+});
