7811c7a3c13679dd7038b88e44e763ae90a34800
Rebecca Weinberger <rew@mit.edu>
2017-01-30 23:08:07 -0500

    scrolly about for all pgs

--- public/scripts/scroll.js
+++ public/scripts/scroll.js
@@ -1,14 +1,33 @@
 $("#scrolltoabout").click(function() {
-  console.log("hi");
+  console.log(":(((((");
   $('#about').css('display', 'block');
   $('html, body').animate({
       scrollTop: $("#about").offset().top
   }, 1500);
 });
 
+$("#closeabout").click(function() {
+  $('html, body').animate({
+      scrollTop: 0
+  }, 1500);
+
+  setTimeout( function(){
+    $('#about').css('display', 'none'); 
+  }  , 1501 );   
+
+
+  // $('#about')
+  // .delay(1501)
+  // .queue(function (next) { 
+  //   $(this).css('display', 'none'); 
+  //   next(); 
+  // });
+  // $('#about').css('display', 'none').delay(1501);
+});
+
 $(".twobuttonsfog").click(function() {
   console.log('hi');
   $('.modal-content').animate({
-      scrollTop: $(".twobuttonsfog").offset().top
+      scrollTop: 0
   }, 800);
 });
